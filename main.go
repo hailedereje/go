0bb2196575cd513bbb1af4b25434846030cdd7af
hailedereje <hailedereje88@gmail.com>
2023-04-14 12:21:47 +0300

    jhi

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
-import (
+import "fmt"
 
-"go_project/routes"
-"go_project/static"
-
-)
+// "go_project/routes"
+// "go_project/static"
 func main() {
 	
-	static.MovieSeeder()
-	routes.Router()	
+	// static.MovieSeeder()
+	// routes.Router()	
+	name := "haile dereje"
+	var ptr *string = &name
+
+	*ptr = "solomon"
+	fmt.Println(name)
 
 }
